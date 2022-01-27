9cffa35ba9113aafe982dd2d0c1897350b30d2a5
m0azam <izmadikram@gmail.com>
2022-01-27 13:16:55 +0800

    Update ErrorResponse.java

--- src/main/java/org/energyweb/ddhub/helper/ErrorResponse.java
+++ src/main/java/org/energyweb/ddhub/helper/ErrorResponse.java
@@ -1,6 +1,5 @@
 package org.energyweb.ddhub.helper;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
@@ -9,17 +8,13 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class ErrorResponse extends Response {
-	@JsonIgnore
-	private Integer tmbulkId;
-
-	public ErrorResponse(String returnCode, String returnMessage, int tmbulkId) {
-		super(returnCode, returnMessage);
-		this.tmbulkId = tmbulkId;
-	}
 
 	public ErrorResponse(String returnCode, String returnMessage) {
 		super(returnCode, returnMessage);
 	}
 
+	public ErrorResponse(String returnCode, String returnMessage) {
+		super(returnCode, returnMessage);
+	}
 
 }

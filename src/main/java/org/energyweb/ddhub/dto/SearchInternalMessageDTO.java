b507f26097149060a0f56637cda96c947b943e3f
azam.ismail <azam.ismail@energyweb.org>
2022-04-13 16:24:14 +0800

    chore : update

--- src/main/java/org/energyweb/ddhub/dto/SearchInternalMessageDTO.java
+++ src/main/java/org/energyweb/ddhub/dto/SearchInternalMessageDTO.java
@@ -52,6 +52,6 @@ public class SearchInternalMessageDTO  {
 		if(Optional.ofNullable(from).isPresent()) {
 			return clientId.concat(Long.toString(from.toEpochSecond(ZoneOffset.UTC))).concat(streamName());
 		}
-		return clientId.concat(streamName());
+		return clientId.concat(LocalDateTime.now().format(DateTimeFormatter.BASIC_ISO_DATE)).concat(streamName());
 	}
 }

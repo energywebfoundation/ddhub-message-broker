cfd5159fb98ea8b333d23721c9978b9318e909ab
azam.ismail <azam.ismail@energyweb.org>
2022-11-15 18:31:58 +0800

    feat: search and send message for registry

--- src/main/java/org/energyweb/ddhub/dto/MessageDTOs.java
+++ src/main/java/org/energyweb/ddhub/dto/MessageDTOs.java
@@ -1,10 +1,9 @@
 package org.energyweb.ddhub.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.Getter;
@@ -16,7 +15,35 @@ import lombok.Setter;
 @NoArgsConstructor
 public class MessageDTOs extends MessageDTO {
     @Valid
-    @NotNull
-	private List<@NotEmpty 	@Size(max = 200, message = "The maximum length is 200 characters") String> fqcns;
+	private List<@Size(max = 200, message = "The maximum length is 200 characters") String> fqcns;
+    
+    @Valid
+    @Size(max = 25, message = "The array size 25 maximum.")
+	private List<@Size(min = 1, max = 255, message = "The length between 1-255 characters") String> anonymousRecipient;
+
+	public boolean anonymousRule() {
+		if(anonymousRecipient == null  && fqcns == null) return true;
+		if((anonymousRecipient != null && anonymousRecipient.isEmpty()) || (fqcns != null && fqcns.isEmpty())) return true;
+		if((anonymousRecipient != null && anonymousRecipient.isEmpty()) && (fqcns != null && fqcns.isEmpty())) return true;
+		return false;
+	}
+
+	public String anonymousRuleErrorMsg() {
+		if(anonymousRecipient == null  && fqcns == null) return "Both fqcns and anonymousRecipient cannot empty.";
+		if((anonymousRecipient != null && anonymousRecipient.isEmpty()) || (fqcns != null && fqcns.isEmpty())) return "Both fqcns and anonymousRecipient cannot empty.";
+		if((anonymousRecipient != null && anonymousRecipient.isEmpty()) && (fqcns != null && fqcns.isEmpty())) return "Both fqcns and anonymousRecipient cannot empty.";
+		return null;
+	}
+
+	public List<String> findFqcnList() {
+		List<String> both = new ArrayList<String>();
+		if((fqcns != null && !fqcns.isEmpty())) {
+			both.addAll(fqcns);
+		}
+		if((anonymousRecipient != null && !anonymousRecipient.isEmpty())) {
+			both.addAll(anonymousRecipient);
+		}
+		return both;
+	}
 
 }
